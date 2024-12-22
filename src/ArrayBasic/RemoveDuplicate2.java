64984ef8fe04a27b4322c9ed547084483496a4e6
Hrishikesh Tiwari <hrishikesht@anthology.com>
2024-12-22 15:24:20 +0530

    Latest_code

--- src/ArrayBasic/RemoveDuplicate2.java
+++ src/ArrayBasic/RemoveDuplicate2.java
@@ -21,7 +21,7 @@ import java.util.TreeSet;
 public class RemoveDuplicate2 {
 
 	public static void main(String[] args) {
-		int arr[]= {1,2,1,3,4,3,5,6,7,8,5,8,8,8,5};
+		int arr[]= {5,5,6,1,1,2,3,2};
 		
 		Set<Integer> set= new HashSet<Integer>();
 		
